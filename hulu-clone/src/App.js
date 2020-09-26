94e63862daa2a483a5790de88adef554f8a01af3
christya95 <christya@sheridancollege.ca>
2020-09-26 16:38:41 -0400

    Created, Header and Nagivation Bar. Utilized Materials UI for icons and added scrolling features. Can toggle display of icon labels with mouse hovering. Included styling as well as a transformation animation for Nav bar items.

---
+++ hulu-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+
+function App() {
+    return ( <div className = "app">
+        <Header />
+        
+        <Nav />
+
+        {/* Results */}
+        </div>
+    );
+}
+
+export default App;
